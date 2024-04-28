d58527440c774d89338c3196cb243257a0d945cc
Batuhan Benli <benlibatuhan6@hotmail.com>
2024-04-28 13:15:20 +0300

    CQRS dosyaları eklendi

---
+++ Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/GetAboutByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.CQRS.Handlers.AboutHandlers
+{
+    public class GetAboutByIdQueryHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public GetAboutByIdQueryHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+    }
+}
--- Infrastructure/CarBook.Persistence/Context/CarBookContext.cs
+++ Infrastructure/CarBook.Persistence/Context/CarBookContext.cs
@@ -12,7 +12,7 @@ namespace CarBook.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-FIQBH8P;initial Catalog=CarBookDb;integrated Security=true;");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-FIQBH8P;initial Catalog=CarBookDb;integrated Security=true;TrustServerCertificate=true");
         }
         public DbSet<About> Abouts { get; set; }
         public DbSet<Banner> Banners { get; set; }
@@ -23,6 +23,12 @@ namespace CarBook.Persistence.Context
         public DbSet<CarPricing> CarPricings { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Contact> Contacts { get; set; }
-        public DbSet<CarPricing> CarPricings { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<FooterAdress> FooterAdresses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Pricing> Pricings { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }
 }
